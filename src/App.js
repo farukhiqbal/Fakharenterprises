438177e5b8c5635f651813b30efe5dc5da1e3130
medcloud <medcloud.pk@gmail.com>
2024-09-11 11:12:45 +0500

    2commit

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
 
-        {/* <Route path="/sitemap" element={<Sitemap />} /> */}
         <Route path="/product" element={<Products />} />
         <Route path="/services" element={<Services />} />
         <Route path="/aboutus" element={<AboutUs />} />
